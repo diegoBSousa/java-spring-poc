f3ba5e469b45487e03db08f5a310d684e7a98057
Diego Sousa <diego@diegosousa.com.br>
2023-08-16 14:25:13 -0300

    Initial Commit

---
+++ src/test/java/com/diegosousa/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegosousa.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
